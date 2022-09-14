8cda708a196efec61b849d1832d03850d1ba5e56
Jessica Ferreira <jessiberteloni@gmail.com>
2022-09-14 15:26:52 -0300

    Calculando descontos

---
+++ exercicios/ex012.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia o preço de um produdo e mostre seu novo preço, com 5% de desconto.
+
+preço = float(input('Qual é o preço do produto? R$'))
+novo = preço - (preço * 5/ 100)
+print('O produto que custava {:.2f}, na promoção com desconto de 5% vai custar {:.2f}'.format(preço, novo))
