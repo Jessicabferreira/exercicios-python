fee53db4b66c6d4fb5e46b6efe63976629c17b81
Jessica Ferreira <jessiberteloni@gmail.com>
2022-09-16 18:10:24 -0300

    Quebrando um número

---
+++ exercicios/ex016.py
@@ -0,0 +1,13 @@
+"""
+ Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+"""
+
+# Exemplo 1
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a porção inteira é {}'.format(num, int(num)))
+print()
+
+# Exemplo 2 com módulo importado
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
