41e9f2de763c24abb2ca6c86b2bd68ebaf8f36e6
Jessica Ferreira <jessiberteloni@gmail.com>
2022-09-17 15:24:29 -0300

    Sorteando um item na lista

---
+++ exercicios/ex019.py
@@ -0,0 +1,11 @@
+""" Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o
+nome dos alunos e escrevendo na tela o nome do escolhido."""
+
+from random import choice  # importamos  a função choice do random randomizador de elementos
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)   # uma escolha dentro da lista
+print('O aluno escolhido foi {}'.format(escolhido))
