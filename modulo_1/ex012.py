e199066fd3615f8008652626bd069b9b0ed546f7
Jessica Ferreira <jessiberteloni@gmail.com>
2022-11-23 17:47:37 -0300

    Calculando descontos

--- modulo_1/ex012.py
+++ modulo_1/ex012.py
@@ -3,3 +3,4 @@
 preço = float(input('Qual é o preço do produto? R$'))
 novo = preço - (preço * 5/ 100)
 print('O produto que custava {:.2f}, na promoção com desconto de 5% vai custar {:.2f}'.format(preço, novo))
+
