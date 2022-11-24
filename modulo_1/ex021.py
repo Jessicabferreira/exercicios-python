f141fc572235e7fb0ee0d6e1a240d44ffde2bf3b
Jessica Ferreira <jessiberteloni@gmail.com>
2022-11-24 14:00:21 -0300

    Tocando um MP3

--- modulo_1/ex021.py
+++ modulo_1/ex021.py
@@ -3,6 +3,7 @@ Faça um programa em python que abra e reproduza o áudio de um arquivo MP3.
 """
 
 import pygame  # instalar o pygame
+
 pygame.init()   # init vai iniciar o uso da biblioteca
 pygame.mixer.music.load('ex021.mp3')  # vai carregar o áudio
 pygame.mixer.music.play()    # vai tocar
