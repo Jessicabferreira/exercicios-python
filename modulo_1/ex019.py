8bf695042d44bc7b0aee96e16ccaeda7a2535490
Jessica Ferreira <jessiberteloni@gmail.com>
2022-11-24 13:54:12 -0300

    Sorteando um item na lista

--- modulo_1/ex019.py
+++ modulo_1/ex019.py
@@ -1,7 +1,9 @@
 """ Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o
 nome dos alunos e escrevendo na tela o nome do escolhido."""
 
-from random import choice  # importamos  a função choice do random randomizador de elementos
+from random import \
+    choice  # importamos  a função choice do random randomizador de elementos
+
 n1 = str(input('Primeiro aluno: '))
 n2 = str(input('Segundo aluno: '))
 n3 = str(input('Terceiro aluno: '))
