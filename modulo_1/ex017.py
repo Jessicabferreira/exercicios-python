58f23b7f4df099c17442e13e454f594a83320963
Jessica Ferreira <jessiberteloni@gmail.com>
2022-11-24 13:53:11 -0300

    Catetos e hipotenusa

--- modulo_1/ex017.py
+++ modulo_1/ex017.py
@@ -12,8 +12,10 @@ print()
 
 # Exemplo 2 método importado
 from math import hypot
+
 co = float(input('Comprimento do cateto oposto: '))
 ca = float(input('Comprimento do cateto adjacente: '))
 hi = hypot(co, ca)
 print('A hipotenusa vai medir {:.2f}'.format(hi))
+
 
