b644a59ba1416d6032113de9286b6b8f0a86e501
Jessica Ferreira <jessiberteloni@gmail.com>
2022-11-24 14:14:17 -0300

    Ano bissexto

--- modulo_1/ex032.py
+++ modulo_1/ex032.py
@@ -2,6 +2,7 @@
 Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 """
 from datetime import date
+
 ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
 if ano == 0:
     ano = date.today().year   # o 0 vai verificar o ano atual
