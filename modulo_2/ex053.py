87ba1effcf4b3c998b1ba7a8483f41d27dfa862c
Jessicabferreira <jessiberteloni@gmail.com>
2023-06-06 17:58:29 -0300

    Detector de Palíndromo

---
+++ modulo_2/ex053.py
@@ -0,0 +1,10 @@
+"""Crie um programa qu eleia uma frase qualquer e diga se ela é um polindromo, desconsiderando os espaços.
+
+Exemplos de palindromos:
+
+apos a sopa
+a sacada da casa
+a torre da derrota
+o lobo ama o bolo
+anotaram a data da maratona
+"""
