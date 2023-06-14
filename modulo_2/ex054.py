58a2a34c692d782b2b557954aa0b50699a120815
Jessicabferreira <jessiberteloni@gmail.com>
2023-06-14 15:58:28 -0300

    Grupo da Maioridade

--- modulo_2/ex054.py
+++ modulo_2/ex054.py
@@ -1,3 +1,8 @@
 """  Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas
  ainda não atingiram a maioridade e quantas já são maiores.
 """
+from datetime import date
+atual = date.today().year
+nasc = int(input("Em que ano a pessoa nasceu? "))
+idade = atual - nasc
+print("Essa pessoa tem {} anos".format(idade))
