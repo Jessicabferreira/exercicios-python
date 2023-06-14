63bd5ff4c4b431b20d872cf50c48cae7b90e3ff2
Jessicabferreira <jessiberteloni@gmail.com>
2023-06-14 16:42:02 -0300

    Grupo da Maioridade

--- modulo_2/ex054.py
+++ modulo_2/ex054.py
@@ -3,6 +3,14 @@
 """
 from datetime import date
 atual = date.today().year
-nasc = int(input("Em que ano a pessoa nasceu? "))
-idade = atual - nasc
-print("Essa pessoa tem {} anos".format(idade))
+totmaior = 0
+totmenor = 0
+for pess in range(1, 8):
+    nasc = int(input("Em que ano {} pessoa nasceu? ".format(pess)))
+    idade = atual - nasc
+    if idade >= 21:
+        totmaior += 1
+    else:
+        totmenor += 1
+print("Ao todo tivemos {} pessoas maiores de idade".format(totmaior))
+print("E também tivemos {} pessoas menores de idade".format(totmenor))
