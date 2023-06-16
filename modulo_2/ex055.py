8a81800c7fb92d786753af41813d9c5c045aba3c
Jessicabferreira <jessiberteloni@gmail.com>
2023-06-16 17:13:24 -0300

    Maior e menor da sequência

--- modulo_2/ex055.py
+++ modulo_2/ex055.py
@@ -1,5 +1,4 @@
 """Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos."""
 
 for p in range(1, 6):
-    peso = float(input("Peso da {} pessoa: ".format()))
-    
+    peso = float(input("Peso da {} pessoa: ".format(p)))
