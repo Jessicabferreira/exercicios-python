4ebf97c80ba0e17a667d73f815f4dc2167368034
Jessicabferreira <jessiberteloni@gmail.com>
2023-06-26 18:40:25 -0300

    Pintando parede

--- modulo_1/ex011.py
+++ modulo_1/ex011.py
@@ -7,4 +7,3 @@ alt = float(input('Altura da parede: '))
 print('Sua parede tem a dimensão de {}x{} e sua área é de {}m².'.format(larg, alt, área))
 tinta = área / 2
 print('Para pintar essa parede, você precisará de {}l de tinta.'.format(tinta))
-
