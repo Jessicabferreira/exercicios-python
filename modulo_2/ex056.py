9e5de40d804df69a1ddb01571d635a5296385e39
Jessicabferreira <jessiberteloni@gmail.com>
2023-06-27 17:50:43 -0300

    Analisador completo

--- modulo_2/ex056.py
+++ modulo_2/ex056.py
@@ -4,12 +4,24 @@ a média de idade do grupo, qual é o nome do homem mais velho e quantas mulhere
 
 somaidade = 0
 médiaidade = 0
+maioridadehomem  = 0
+nomevelho = ""
+totmulher20 = 0
 for p in range(1, 5):
     print("----- {}ª PESSOA -----".format(p))
     nome = str(input("Nome: ")).strip()
     idade = int(input("Idade: "))
     sexo = str(input("Sexo [M/F]: ")).strip()
     somaidade += idade
-    
+    if p == 1 and sexo in "Mm":
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in "Mm" and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in "Ff" and idade < 20:
+        totmulher20 += 1
 médiaidade = somaidade / 4
 print("A mádia de idade do grupo é de {} anos".format(médiaidade))
+print('O homem mais velho tem {} anos e se chama {}'.format(maioridadehomem, nomevelho))
+print("Ao todo são {} mulheres com menos de 20 anos".format(totmulher20))
