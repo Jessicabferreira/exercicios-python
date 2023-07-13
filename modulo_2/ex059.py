ec6ccccb967f9574ae0178c1c0252cb0fa524e76
Jessicabferreira <jessiberteloni@gmail.com>
2023-07-13 19:24:33 -0300

    Criando um Menu de Opções

--- modulo_2/ex059.py
+++ modulo_2/ex059.py
@@ -16,7 +16,7 @@ n1 = int(input("Primeiro valor: "))
 n2 = int(input("Segudo valor: "))
 opção = 0
 while opção != 5:
-    print('''     [1] somar
+    print('''      [1] somar
       [2] multiplicar
       [3] maior
       [4] novos números
@@ -33,7 +33,7 @@ while opção != 5:
             maior = n1
         else: 
             maior = n2
-            print("Entre {} e {} o maior valor é {}".format(n1, n2, maior))
+        print("Entre {} e {} o maior valor é {}".format(n1, n2, maior))
     elif opção == 4:
         print("Informe os números novamente:")
         n1 = int(input("Primeiro valor:"))
