e354103605e49b47f02b280378d15d85f01da764
Jessicabferreira <jessiberteloni@gmail.com>
2023-07-14 16:24:25 -0300

    Cálculo do Fatorial

--- modulo_2/ex060.py
+++ modulo_2/ex060.py
@@ -1,2 +1,7 @@
 """Faça um programa que leia um número qualquer e mostre o seu fatorial
-Exemplo:  5!=5x4x3x2x1=120"""
+Exemplo:  5!= 5x4x3x2x1= 120"""
+
+from math import factorial
+n = int(input("Digite um número para calcular seu Fatorial: "))
+f = factorial(n)
+print("O fatorial de {} é {}.".format(n, f))
