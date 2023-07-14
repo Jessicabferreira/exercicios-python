ba7db16ded1f0e536f13e52638e530ea82641b37
Jessicabferreira <jessiberteloni@gmail.com>
2023-07-14 16:39:31 -0300

    Cálculo do Fatorial

--- modulo_2/ex060.py
+++ modulo_2/ex060.py
@@ -3,7 +3,11 @@ Exemplo:  5!= 5x4x3x2x1= 120"""
 
 n = int(input("Digite um número para calcular seu Fatorial: "))
 c = n
+f = 1
+print("Calculando {}! = ".format(n), end='')
 while c > 0:
-    print("{} x ".format(c), end=' ')
+    print(" {} ".format(c), end='')
+    print("x" if c > 1 else ' = ', end='')
+    f *= c
     c -= 1
-# print("O fatorial de {} é {}.".format(n, f))
+print("{}".format(f))
