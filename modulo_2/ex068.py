96a2335657fed05c2b490d9e524bd305cb84780b
Jessicabferreira <jessiberteloni@gmail.com>
2023-09-13 18:09:57 -0300

    Jogo do Par ou Ímpar

--- modulo_2/ex068.py
+++ modulo_2/ex068.py
@@ -6,14 +6,27 @@ o total de vitórias consecutivas que ele conquistou no final do jogo.
 from random import randint
 
 while True:
-    jogador = int(input('Diga um valor'))
+    jogador = int(input('Diga um valor: '))
     computador = randint(0, 10)
     total = jogador + computador
-    tipo = ''
+    tipo = ' '
     while tipo not in 'PI':
-        tipo = str(input('Par ou impar? [P/I] ')).strip().upper() [0]
+        tipo = str(input('Par ou Ìmpar? [P/I] ')).strip().upper() [0]
     print(f'Você jogou {jogador} e o computador {computador}. Total de {total}')
-    if tipo == 'p':
+    print('DEU PAR' if total % 2== 0 else 'DEU IMPAR')
+    if tipo == 'P':
         if total % 2 == 0:
             print('Você VENCEU!')
-            
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+        print('Vamos jogar novamente...')
+    print('GAME OVER! Você venceu {} vezes.')
