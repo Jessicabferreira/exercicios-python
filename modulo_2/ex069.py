369f8b53afeedbf499a72aec9722a9ce5b4cfba5
Jessica Berteloni Ferreira <jessiberteloni@gmail.com>
2024-01-10 15:48:14 -0300

    Análise de dados do grupo

--- modulo_2/ex069.py
+++ modulo_2/ex069.py
@@ -8,3 +8,15 @@ B) quantos homens foram cadastrados.
 
 C) quantas mulheres tem menos de 20 anos
 """
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+            break
+print('Acabou!')
