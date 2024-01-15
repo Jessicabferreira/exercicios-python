cd1cbe98298d0b33a03053a48ac96ddaaaaa47e6
Jessica Berteloni Ferreira <jessiberteloni@gmail.com>
2024-01-15 16:24:34 -0300

    Número por Extenso

--- modulo_3/ex072.py
+++ modulo_3/ex072.py
@@ -1,3 +1,5 @@
 """ 
-Número por Extenso
+Crie um programa que temha uma dupla totalmente preenchida com uma contagem
+por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado
+(entre 0 e 20) e mostrá-lo por extenso.
 """
