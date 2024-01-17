42481752c47c60986a26c969889b38e9f0f0d462
Jessica Berteloni Ferreira <jessiberteloni@gmail.com>
2024-01-17 15:43:54 -0300

    Tuplas com Times de Futebol

--- modulo_3/ex073.py
+++ modulo_3/ex073.py
@@ -1,3 +1,13 @@
 """
-Tuplas com times de futebol 
-"""
+Crie uma tupla preenchida com os 20 primeiros colococados da
+Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação.
+Depois mostre:
+ 
+a) Os 5 primeiros times.
+
+b) Os últimos 4 colocados.
+
+c) Times em ordem alfabética.
+
+d) Em que posição está o time da Chapecoense.
+ """
