2515212c21fe7b24e6a98dba1170ce8caf30d3a7
Jessica Berteloni Ferreira <jessiberteloni@gmail.com>
2024-01-22 17:50:52 -0300

    Tuplas com Times de Futebol

--- modulo_3/ex073.py
+++ modulo_3/ex073.py
@@ -11,3 +11,20 @@ c) Times em ordem alfabética.
 
 d) Em que posição está o time da Chapecoense.
  """
+
+times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio',
+         'Cruzerio', 'Flamengo', 'Vasvo', 'Chapecoense',
+         'Atlético', 'Botafogo', 'Atlético-PR', 'Bahia',
+         'São Paulo', 'Fluminense', 'Sport Recife',
+         'EC Vítória', 'Coritiba', 'avaí', 'Ponte Preta',
+         'Atlético-GO')
+print('-=' * 15)
+print(f'Lista de times do Brasileirão: {times}')
+print('-=' * 15)
+print(f'Os 5 primeiros são {times[0:5]}')
+print('-=' * 15)
+print(f'Os 4 últimos são {times[-4:]}')
+print('-=' * 15)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-=' * 15)
+print(f'O chapecoense está na {times.index("Chapecoense")+1}ª posicão')
